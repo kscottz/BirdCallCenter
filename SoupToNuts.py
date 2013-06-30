4c0b53c2823885a29eef23884b18e68ece61371a
kscottz <kat@sightmachine.com>
2013-06-30 17:22:47 -0400

    okay about to start the big scrape

--- SoupToNuts.py
+++ SoupToNuts.py
@@ -1,13 +1,62 @@
 from BeautifulSoup import BeautifulSoup 
 import urllib2
 import os
-url = urllib2.urlopen("http://www.xeno-canto.org/browse.php?species_nr=&query=Cyanocitta+cristata")
-content = url.read()
-soup = BeautifulSoup(content)
-found = soup.findAll("div", {"class": "xc-button-audio"})
-for foundSound in found:
-    soundUrl = [f[1] for f in foundSound.attrs if f[0]==u'data-xc-filepath']
-    grabThis = "wget "
-    grabThis = grabThis + soundUrl[0]
-    print soundUrl[0]
-    os.system(grabThis)
+
+dataPath = 'rawData'
+
+birdList = [
+    'Cyanocitta+cristata',#blue jay
+    'Cardinalis+cardinalis',#cardinal
+    'Zenaida+macroura', #morning dove
+    'Turdus+migratorius',#robin
+    'Carduelis+tristis', #gold finch
+    'Passexu+domesticus', #house sparrow
+    'Quiscalus+quiscula', #common grackle
+    'Archilochus+colubris', #ruby throated humming bird
+    'Picoides+pubescens', # downy wood pecker
+    'Columba+livia', # rock pigeon
+    'Sitta+carolinensis', # white breasted nut-hatch
+    'Poecile+atricapillus', # Black-capped chickadee
+    'Agelaius+phoeniceus', # Red-winged black bird
+    'Haemorhous+purpureus', #Purple Finch
+    'Baeolophus+bicolor', #tufted titmouse
+    ]
+
+
+def downloadBirdCalls(stub, bird,path):
+    print 30*"$"
+    print path
+    os.chdir(path)
+    url = stub+bird
+    url = urllib2.urlopen(url)
+    content = url.read()
+    soup = BeautifulSoup(content)
+    found = soup.findAll("div", {"class": "xc-button-audio"})
+    for foundSound in found:
+        soundUrl = [f[1] for f in foundSound.attrs if f[0]==u'data-xc-filepath']
+        grabThis = "wget "
+        grabThis = grabThis + soundUrl[0]
+        print soundUrl[0]
+        #os.system(grabThis)
+
+
+
+if not os.path.exists(os.path.join(os.getcwd(), dataPath)):
+    os.mkdir(os.path.join(os.getcwd(),dataPath))
+
+os.chdir(os.path.join(os.getcwd(),dataPath))
+
+paths = []
+
+for bird in birdList:
+    if not os.path.exists(os.path.join(os.getcwd(), bird)):
+        os.mkdir(os.path.join(os.getcwd(),bird))
+        print "Creating " + os.path.join(os.getcwd(),bird)
+    else:
+        print "Found " + os.path.join(os.getcwd(),bird)
+    paths.append(os.path.join(os.getcwd(),bird))
+
+stub = "http://www.xeno-canto.org/browse.php?species_nr=&query="
+
+for bird,path in zip(birdList,paths):
+    downloadBirdCalls(stub, bird,path)
